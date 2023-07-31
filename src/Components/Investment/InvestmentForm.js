f0938b85ded42698bf7b774060e08d7875dc53b7
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-07-31 21:51:09 +0300

    logic Added

--- src/Components/Investment/InvestmentForm.js
+++ src/Components/Investment/InvestmentForm.js
@@ -1,16 +1,92 @@
+import React,{useState} from "react";
 import styles from "./InvestmentForm.module.css";
 
 function InvestmentForm(props){
+  
+  
+    const [currentSavings,setCurrentSavings] = useState(0);
+    const [yearlyContribution,setYearlyContribution] = useState(0);
+    const [expectedReturn,setExpectedReturn] = useState(0);
+    const [duration,setDuration] = useState(0);
+
+    const calculateHandler = (userInput) => {
+        // Should be triggered when form is submitted
+        // You might not directly want to bind it to the submit event on the form though...
+    
+        const yearlyData = []; // per-year results
+    
+        let currentSavings = +userInput['currentSavings']; // feel free to change the shape of this input object!
+        const yearlyContribution = +userInput['yearlyContribution']; // as mentioned: feel free to change the shape...
+        const expectedReturn = +userInput['expectedReturn'] / 100;
+        const duration = +userInput['duration'];
+    
+        // The below code calculates yearly results (total savings, interest etc)
+        for (let i = 0; i < duration; i++) {
+          const yearlyInterest = currentSavings * expectedReturn;
+          currentSavings += yearlyInterest + yearlyContribution;
+          yearlyData.push({
+            // feel free to change the shape of the data pushed to the array!
+            year: i + 1,
+            yearlyInterest: +yearlyInterest,
+            savingsEndOfYear: +currentSavings,
+            yearlyContribution: +yearlyContribution,
+          });
+        }
+        
+        // do something with yearlyData ...
+      
+        return yearlyData;
+    };
+
+    function formSubmitionHandler(event){
+        event.preventDefault();
+     
+        
+        const userInput = {
+            currentSavings : +currentSavings,
+            yearlyContribution : +yearlyContribution,
+            expectedReturn : +expectedReturn,
+            duration : +duration
+        }
+        
+        const results = calculateHandler(userInput);
+        
+        props.onFormSubmited(results,userInput.currentSavings);
+    }
+    
+    function currentSavingsChangedHandler(event){
+        setCurrentSavings(event.target.value);
+    }
+    function yearlyContributionChangedHandler(event){
+        setYearlyContribution(event.target.value);
+    }
+    function expectedReturnChangedHandler(event){
+        setExpectedReturn(event.target.value);
+    }
+    function durationChangedHandler(event){
+        setDuration(event.target.value);
+    }
+  
+    function formResetHandler(){
+        setCurrentSavings(0);
+        setYearlyContribution(0);
+        setExpectedReturn(0);
+        setDuration(0);
+        props.onFormSubmited(null,null);
+    }
+  
+  
+  
     return (
         <form className={styles.form}>
         <div className={styles["input-group"]}>
           <p>
             <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
+            <input onChange={currentSavingsChangedHandler} type="number" id="current-savings" />
           </p>
           <p>
             <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
+            <input onChange={yearlyContributionChangedHandler} type="number" id="yearly-contribution" />
           </p>
         </div>
         <div className={styles["input-group"]}>
@@ -18,18 +94,18 @@ function InvestmentForm(props){
             <label htmlFor="expected-return">
               Expected Interest (%, per year)
             </label>
-            <input type="number" id="expected-return" />
+            <input onChange={expectedReturnChangedHandler} type="number" id="expected-return" />
           </p>
           <p>
             <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
+            <input onChange={durationChangedHandler} type="number" id="duration" />
           </p>
         </div>
         <p className={styles.actions}>
-          <button type="reset" className={styles.buttonAlt}>
+          <button onClick={formResetHandler} type="reset" className={styles.buttonAlt}>
             Reset
           </button>
-          <button type="submit" className={styles.button}>
+          <button onClick={formSubmitionHandler} type="submit" className={styles.button}>
             Calculate
           </button>
         </p>
--- src/Components/Investment/InvestmentResults.js
+++ src/Components/Investment/InvestmentResults.js
@@ -1,6 +1,18 @@
 import styles from "./InvestmentResults.module.css";
-
 function InvestmentResults(props){
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+
+      if(props.results.length === 0){
+        return <p style={{textAlign: 'center'}}>No investment calculated yet.</p>;
+      }
+
+
     return (
         <table className={styles.result}>
         <thead>
@@ -13,13 +25,26 @@ function InvestmentResults(props){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
+          {props.results.map((result) => 
+            <tr key={result.year}>
+            <td>{result.year}</td>
+            <td>{formatter.format(result.savingsEndOfYear)}</td>
+            <td>{formatter.format(result.yearlyInterest)}</td>
+            <td>
+              {formatter.format(
+                result.savingsEndOfYear -
+                  props.initialInvestment -
+                  result.yearlyContribution * result.year
+              )}
+            </td>
+            <td>
+              {formatter.format(
+                props.initialInvestment +
+                result.yearlyContribution * result.year
+              )}
+            </td>
           </tr>
+          )}
         </tbody>
       </table>
     )
